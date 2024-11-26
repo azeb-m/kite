d7a25b3be2ef9ba5a8652470eb46f52a1fdd7642
Sosna-mehrete <sosnamehrete@gmail.com>
2024-11-26 14:58:12 +0300

    about us page

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port=3000;
+const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 // Routes
-app.get('/', (req, res) => {
-    res.render("index.ejs",{ title: 'Your Website Title' });
+app.get("/", (req, res) => {
+  res.render("index.ejs", { title: "Your Website Title" });
+});
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
 });
 
 //app.get('/about', (req, res) => {
-   // res.render('about', { title: 'About Us' });
+// res.render('about', { title: 'About Us' });
 //});
 
 //app.get('/contact', (req, res) => {
- //   res.render('contact', { title: 'Contact Us' });
+//   res.render('contact', { title: 'Contact Us' });
 //});
 
 // Start the server
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
